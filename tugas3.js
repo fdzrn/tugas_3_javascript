fed93d93a0d17eea86bf9537a4bb6a88657d179b
fdzrn <fadzriangibran@gmail.com>
2019-07-31 14:27:12 +0700

    Tugas 3 Javascript
    Tugas ini saya kirimkan untuk Niomic.com

---
+++ tugas3.js
@@ -0,0 +1,7 @@
+function tugasTiga(x) {
+	let angka = 10; // variabel awal
+	console.log(angka);
+	return angka = x; // nilai variabel di kembalikan berdasarkan isi dari parameter yang di input
+}
+
+console.log(tugasTiga(20));
